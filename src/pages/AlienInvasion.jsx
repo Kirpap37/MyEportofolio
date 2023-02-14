9ef774fed8909c32365b8bac3c18b46fed4e93be
Kiriakos Papadopoulos <Kirpap37@yahoo.gr>
2023-02-14 18:55:31 +0200

    sixth

--- src/pages/AlienInvasion.jsx
+++ src/pages/AlienInvasion.jsx
@@ -74,6 +74,32 @@ function AlienInvasion() {
          Alien1,Alien2,Alien3,Alien4,Alien5,Alien6,Alien7,Alien8,Alien9,Alien10
         ]}
       />
+      <div className='gameAbout'>
+        <p>
+          <strong>Alien Invasion</strong> is a space shoot 'em up game.
+          It is made using Scratch and you can play it only by using the 
+          movement of your mouse, as it shoots automatically.
+          It has two modes, Story mode and Infinite mode 
+        </p>
+        <p>
+         In Story mode, you have to stay alive until the end and kill the 
+         final boss, to save the planet from the Alien Invasion. You have 
+         3 lives to do it, that you can lose by getting hit with a bullet 
+         or colliding with an enemy. By killing enemies, you have the chance
+         to get a Power up. There are  2 types of Power ups. The first one
+         is the Rapid fire, which allows you to shoot very fast like a machine gun,
+         and the other one is the Shield that prevents you from taking any kind of damage.
+        </p>
+        <p>
+         In Infinite mode, you have to stay alive as long as you can, so you
+         can raise your high score. In this mode, you only have one life. 
+         By staying alive you are getting 2 points every second and by killing an 
+         enemy 10 points. The previous power ups are available in this mode, too.
+        </p>
+        <p>
+        You can play the game at<a href='https://scratch.mit.edu/projects/407620172' target='blank'>https://scratch.mit.edu/projects/407620172.</a> 
+        </p>
+      </div>
     </div>
   );
 }
